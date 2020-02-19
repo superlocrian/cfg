c61c707ff610b42bf80babf8c1a5b5b03610a64c
Жаров Александр <a.zharov@corp.mail.ru>
2020-02-19 17:09:10 +0300

    upd readme and examples

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # CFG
 
 A simple configurator that parses go-tags and fills the container passed to it with the values of flags and environment variables.
-The flags values overwrite the default values contained in the transferred container. Environment variables overwrite default values and flags values.
+The flags values overwrite the default values, contained in the transferred container. Environment variables overwrite default values and flags values.
+It is useful when your application has to use different configuration sources.
+
+Go tag format for commandline flags:
+```golang
+type YourConfig struct {
+	Field   int    `cmd:"flagname|flag description string"`
+}
+```
+Go tag format for environment variables:
+```golang
+type YourConfig struct {
+	Field   int    `env:"VARNAME_WITHOUT_PREFIX"`
+}
+```
+
+### limitations:
+
+* So far, there is no way to set both normal and shortened flag names for the same field.
+* It is not possible to set up default values for commandline flags using go tags (but it would be cool to add)
+
+
--- examples/basic.go
+++ examples/basic.go
@@ -11,11 +11,12 @@ import (
 )
 
 type Config struct {
-	IntVal   int    `yaml:"intval" cmd:"intval|integer value" env:"INTVAL"`
-	Int64    int64  `yaml:"int64"  cmd:"int64|integer 64 value " env:"INT64"`
-	StrVal   string `yaml:"strval" cmd:"strval" env:"STRVAL"`
-	IntSlice []int  `yaml:"int-slice" cmd:"intslice" env:"INTSLICE"`
-	Data     struct {
+	ConfigFilePath string `cmd:"c|path to config.yml"`
+	IntVal         int    `yaml:"intval" cmd:"intval|integer value" env:"INTVAL"`
+	Int64          int64  `yaml:"int64"  cmd:"int64|integer 64 value " env:"INT64"`
+	StrVal         string `yaml:"strval" cmd:"strval" env:"STRVAL"`
+	IntSlice       []int  `yaml:"int-slice" cmd:"intslice" env:"INTSLICE"`
+	Data           struct {
 		Fl64    float64 `yaml:"fl-64" cmd:"float|how to use float" env:"FL64"`
 		BoolVal bool    `yaml:"bool-val" cmd:"bool|boolean value" env:"BOOL"`
 	}
@@ -32,8 +33,11 @@ func (o *Config) FromYamlFile(file string) (err error) {
 
 func main() {
 
-	conf := &Config{}
-	if err := conf.FromYamlFile("config.yml"); err != nil {
+	conf := &Config{
+		ConfigFilePath: "config.yml",
+		IntVal:         123, //default value should be rewritten by env variable or cmd flag or yaml config depending of what you set up
+	}
+	if err := conf.FromYamlFile(conf.ConfigFilePath); err != nil {
 		log.Fatal(err)
 	}
 	if err := cfg.Unmarshal(conf, "", flag.CommandLine, os.Args[1:]); err != nil {
@@ -41,5 +45,8 @@ func main() {
 	}
 
 	fmt.Printf("%+v\n", conf)
+	//run example:
+	//$ go build basic.go
+	//$ INTVAL=12345 ./basic -intval 1234
 
 }
